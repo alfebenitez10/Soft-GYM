12ee6091d3900f68eed335407592115754225058
Alfe Benitez <joseangel191134@gmail.com>
2020-09-03 19:21:36 -0500

    Add to repository

---
+++ database/migrations/2020_09_03_235511_create_registros_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegistrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registros', function (Blueprint $table) {
+            $table->id('cod_registro')->autoIncrement();
+            $table->dateTime('hora_entrada');
+            $table->timestamps();
+            $table->foreign('cod_registro')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registros');
+    }
+}
